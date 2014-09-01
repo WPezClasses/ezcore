4d8784e6ec456e3d6d6f5b49a88cc0e0286d2253
alchemyunited <mark.simchock@alchemyunited.com>
2014-09-01 07:44:38 -0400

    more static methods

--- class-wp-ezclasses-ezcore-wp-enqueue/class-wp-ezclasses-ezcore-wp-enqueue.php
+++ class-wp-ezclasses-ezcore-wp-enqueue/class-wp-ezclasses-ezcore-wp-enqueue.php
@@ -20,48 +20,177 @@
  */
  
 if ( ! class_exists('Class_WP_ezClasses_ezCore_WP_Enqueue') ) {
-	class Class_WP_ezClasses_ezCore_WP_Enqueue extends Class_WP_ezClasses_Master_Singleton {
+  class Class_WP_ezClasses_ezCore_WP_Enqueue extends Class_WP_ezClasses_Master_Singleton {
+  
+    protected $_arr_init;
 	
-		protected $_bool_validate_only_active_true;
-		protected $_arr_wp_enqueue_web_fonts;
-		protected $_arr_wp_enqueue_scripts_and_styles;
+	public function __construct() {
+	  parent::__construct();
+	}
+		
+    public function ezc_init($arr_args = NULL){
+      $this->_arr_init = $this->init_defaults();
+
+	}
+		
+    protected function init_defaults(){
+	
+	  $arr_defaults = array(
+	    'active' 		=> true,
+		'active_true'	=> true,
+		'filters' 		=> false,
+		'validation' 	=> false,
+		'arr_args'		=> array(),
+        ); 
+	  return $arr_defaults;
+	}
+	
+	/**
+	 * wp_register_script + wp_register_style
+	 */ 
+    public function ez_rs($arr_args = ''){
+	
+	  if ( ! WP_ezMethods::array_pass($arr_args) ){
+	    return array('status' => false, 'msg' => 'ERROR: arr_args is not valid', 'source' => get_class() . ' ' . __METHOD__, 'arr_args' => 'error');
+	  }
+
+	    $arr_args = WP_ezMethods::ez_array_merge(array( $this->_arr_init, $arr_args)); 
+	
+	    if ( $arr_args['active'] === true && WP_ezMethods::array_pass($arr_args['arr_args']) ){
+		
+		  $arr_wp_register = $arr_args['arr_args'];
+
 			
-		public function __construct() {
-			parent::__construct();
-		}
-		
-		public function ezc_init($arr_args = NULL){
-			$this->theme_wp_enqueue_init($arr_args);
-		}
-		
-		/**
-		 *
-		 */
-		public function theme_wp_enqueue_init($arr_args = NULL){
-		
-			// TODO - get this property with a set()
-			$arr_defaults = $this->theme_wp_enqueue_init_defaults();
-			$this->_bool_validate_only_active_true = $arr_defaults['bool_validate_only_active_true'];
-			if ( isset($arr_args['bool_validate_only_active_true']) && is_bool($arr_args['bool_validate_only_active_true']) ){
-				$this->_bool_validate_only_active_true = $arr_args['bool_validate_only_active_true'];
-			}
-		}
-		
-		protected function theme_wp_enqueue_init_defaults(){
-		
-			$arr_defaults = array(
-								'bool_validate_only_active_true' => true,
-							);
-			return $arr_defaults;
-		}
+				// validate - optional
+				/* TODO
+				if ( isset($arr_args['validate']) && $arr_args['validate'] === true ){
+					$bool_return = false;
+					$arr_validate_response_merge = array();
+					
+					$arr_validate_response = $this->_obj_ezc_theme_register_sidebar->register_sidebar_base_validate($arr_register_sidebar_base);
+					if ( $arr_validate_response['status'] !== true){
+						$arr_validate_response_merge[] = $arr_validate_response;
+						$bool_return = true;
+					} else {
+						$arr_register_sidebar_base = $arr_validate_response['arr_args'];
+					}
+					
+					
+					$arr_validate_response = $this->_obj_ezc_theme_register_sidebar->register_sidebar_validate($arr_register_sidebar);
+					if ( $arr_validate_response['status'] !== true){
+						$arr_validate_response_merge[] = $arr_validate_response;
+						$bool_return = true;
+					} else {
+						$arr_register_sidebar = $arr_validate_response['arr_args'];
+					}
+					
+					if  ( $bool_return === true ) {
+						return $arr_validate_response_merge;
+					}	
+				}
+				*/
+							
+				/**
+				 * returns arr_args that are active. if you don't really have an active => false then checking this is not really necessary (but it can help). 
+				 */
+				if ( WP_ezMethods::ez_true($arr_args['active_true']) ){
+				
+					$arr_active_true_response = $this->wp_register_enqueue_active_true($arr_wp_register);
+					
+					if ( $arr_active_true_response['status'] === false ){
+						return $arr_active_true_response;
+					}
+					$arr_wp_register = $arr_active_true_response['arr_args'];
+				}
+				
+				/**
+				 * At this point we should be good to go. Do!
+				 */ 
+				 
+				$this->wp_register_do($arr_wp_register);
 
-/*
- * ===============================================================================================
- * ==> wp_enqueue
- * ===============================================================================================
- */	
+				return true;
+			
+		} else {
+			//TODO - not an array
+		}
+	}  
+	
+	/**
+	 * wp_enqueue_script + wp_enqueue_style
+	 */ 
+    public function ez_es($arr_args = ''){
+	
+	  if ( ! WP_ezMethods::array_pass($arr_args) ){
+	    return array('status' => false, 'msg' => 'ERROR: arr_args is not valid', 'source' => get_class() . ' ' . __METHOD__, 'arr_args' => 'error');
+	  }
+	  
+	    $arr_args = WP_ezMethods::ez_array_merge(array( $this->_arr_init, $arr_args)); 
+	
+	    if ( $arr_args['active'] === true && WP_ezMethods::array_pass($arr_args['arr_args']) ){
+		
+		  $arr_wp_enque = $arr_args['arr_args'];
+
+			
+				// validate - optional
+				/* TODO
+				if ( isset($arr_args['validate']) && $arr_args['validate'] === true ){
+					$bool_return = false;
+					$arr_validate_response_merge = array();
+					
+					$arr_validate_response = $this->_obj_ezc_theme_register_sidebar->register_sidebar_base_validate($arr_register_sidebar_base);
+					if ( $arr_validate_response['status'] !== true){
+						$arr_validate_response_merge[] = $arr_validate_response;
+						$bool_return = true;
+					} else {
+						$arr_register_sidebar_base = $arr_validate_response['arr_args'];
+					}
+					
+					
+					$arr_validate_response = $this->_obj_ezc_theme_register_sidebar->register_sidebar_validate($arr_register_sidebar);
+					if ( $arr_validate_response['status'] !== true){
+						$arr_validate_response_merge[] = $arr_validate_response;
+						$bool_return = true;
+					} else {
+						$arr_register_sidebar = $arr_validate_response['arr_args'];
+					}
+					
+					if  ( $bool_return === true ) {
+						return $arr_validate_response_merge;
+					}	
+				}
+				*/
+							
+				/**
+				 * returns arr_args that are active. if you don't really have an active => false then checking this is not really necessary (but it can help). 
+				 */
+				if ( WP_ezMethods::ez_true($arr_args['active_true']) ){
+				
+					$arr_active_true_response = $this->wp_register_enqueue_active_true($arr_wp_enqueue);
+					
+					if ( $arr_active_true_response['status'] === false ){
+						return $arr_active_true_response;
+					}
+					$arr_wp_enqueue = $arr_active_true_response['arr_args'];
+				}
+				
+				/**
+				 * At this point we should be good to go. Do!
+				 */ 
+				$this->wp_enqueue_do($arr_wp_register);
+
+				return true;
+			
+		} else {
+			//TODO - not an array
+		}
+	}  
 
 
+
+
+
+/* TODO
 		protected function wp_enqueue_validate($arr_args = NULL){
 			$str_return_source = 'Class_WP_ezClasses_Core_WP_Enqueue :: wp_enqueue_validate()'; 
 
@@ -130,11 +259,13 @@ if ( ! class_exists('Class_WP_ezClasses_ezCore_WP_Enqueue') ) {
 				return array('status' => false, 'msg' => $arr_msg, 'source' => $str_return_source, 'arr_args' => 'error');
 			}
 		} 
+*/
 		
 		/*
 		*
 		*/
-		static public function wp_enqueue_css_media_supported(){
+		/*
+		public function wp_enqueue_css_media_supported(){
 		
 			return array(
 						'all' 			=> true,
@@ -149,171 +280,125 @@ if ( ! class_exists('Class_WP_ezClasses_ezCore_WP_Enqueue') ) {
 						'tv'			=> true,
 					);
 		}
+		*/
+		
+		public function wp_enqueue_script_defaults(){
+		
+		  return array(
+		    'active'			=> true,
+			'conditional_tags'	=> array(),
+			'type'				=> 'required',  //legit values are 'script' and 'style'. if type is required the _do will bypass it. 
+		    'src'				=> false,
+			'deps'				=> array(),
+			'ver'				=> false,
+			'media'				=> 'all',
+			'in_footer'			=> false
+			);
+		}
 		
 		
 
 		/**
 		 * 
 		 */
-		public function wp_enqueue_active_true($arr_args = NULL) {
-			$str_return_source = 'Class_WP_ezClasses_Core_WP_Enqueue :: wp_enqueue_active_true()'; 
-
+		public function wp_register_enqueue_active_true($arr_args = '') {
+		  $str_return_source = get_class() . ' ' . __METHOD__;
 		
-			if ( is_array($arr_args) && !empty($arr_args) ) {
+			if ( WP_ezMethods::array_pass($arr_args) ) {
 				$arr_active_true = array();	
 				foreach ( $arr_args as $str_key => $arr_value ) {
-					if ( $arr_value['active'] === true){
+					if ( WP_ezMethods::ez_true($arr_value['active'] === true) ){
 						$arr_active_true[$str_key] = $arr_value;
 					}
 				}
 				return array('status' => true, 'msg' => 'success', 'source' => $str_return_source, 'arr_args' => $arr_active_true);
 			} 
-			return array('status' => false, 'msg' => 'ERROR: arr_args[] == ( ! is_array() || empty() )', 'source' => $str_return_source, 'arr_args' => 'error');
+			return array('status' => false, 'msg' => 'ERROR: arr_args is not valid', 'source' => $str_return_source, 'arr_args' => 'error');
 		}
 
 		
 		/**
 		 * 
 		 */		
-		public function wp_enqueue_do($arr_args){
-			$str_return_source = 'Class_WP_ezClasses_Core_WP_Enqueue :: wp_enqueue_do()'; 
+		public function wp_register_do($arr_args = ''){
+		  $str_return_source = get_class() . ' ' . __METHOD__;
 
-			if ( is_array($arr_args) ){
-				$obj_general_conditional_tags = Class_WP_ezClasses_ezCore_Conditional_Tags::ezc_get_instance();
+			if ( WP_ezMethods::array_pass($arr_args) ){
 				
 				foreach ( $arr_args as $str_key => $arr_value ) {
-
-					$arr_ret = $obj_general_conditional_tags->conditional_tags_evaluate($arr_value['conditional_tags']);
-					if ( $arr_ret['status'] === true ){
+				
+				  // we'll slide the defaults underneath just in case
+				  $arr_value = WP_ezMethods::ez_array_merge(array($this->wp_enqueue_script_defaults(), $arr_value));
+				
+				  // validation is a TODO
+				  if ( $arr_value['active'] === true ){
+				
+					// $arr_ret = $obj_ezcore_conditional_tags->conditional_tags_evaluate($arr_value['conditional_tags']);
+					/// if ( $arr_ret['status'] === true ){
 						if ( $arr_value['type'] == 'style' ){
 						
 							wp_register_style($arr_value['handle'], $arr_value['src'], $arr_value['deps'] ,$arr_value['ver'], $arr_value['media'] );
-							wp_enqueue_style($arr_value['handle']);
-							
+		//	echo '<br>' .$arr_value['handle'] . $arr_value['src'] . ' - ' . $arr_value['deps'] . ' - ' . $arr_value['ver'] . $arr_value['media'] . '</br>';
 						} elseif ( $arr_value['type'] == 'script' ){
+		//	echo '<br>' .$arr_value['handle'] . $arr_value['src'] . ' - ' . $arr_value['deps'] . ' - ' . $arr_value['ver'] . $arr_value['in_footer'] . '</br>';
+		
+							wp_register_script($arr_value['handle'], $arr_value['src'], $arr_value['deps'] ,$arr_value['ver'], $arr_value['in_footer'] );
+		//	echo '<br> script </br>';
+						} else {
+						  // TODO - style must have been invalid. 
+						}
+		//print_r($arr_value);
+					//}
+				  }
+				}
+//die('c');				
+				return array('status' => true, 'msg' => 'success', 'source' => $str_return_source, 'arr_args' => $arr_args);
+			}
+			return array('status' => false, 'msg' => 'ERROR: arr_args !is_array()', 'source' => $str_return_source, 'arr_args' => 'error');
+		}
+		
+		/**
+		 * 
+		 */		
+		public function wp_enqueue_do($arr_args = ''){
+		  $str_return_source = get_class() . ' ' . __METHOD__;
+
+			if ( WP_ezMethods::array_pass($arr_args) ){
+			
+				$obj_ezcore_conditional_tags = Class_WP_ezClasses_ezCore_Conditional_Tags::ezc_get_instance();
+				$arr_wp_enqueue_script_defaults = $this->wp_enqueue_script_defaults();
+				
+				foreach ( $arr_args as $str_key => $arr_value ) {
+				
+				  $arr_value = array_merge($arr_wp_enqueue_script_defaults,  $arr_value);
+				
+				  // validation is a TODO
+				  if ( $arr_value['active'] === true ){
+				
+					$arr_ret = $obj_ezcore_conditional_tags->conditional_tags_evaluate($arr_value['conditional_tags']);
+					if ( $arr_ret['status'] === true ){
+				//	echo $arr_value['handle'];
+						if ( wp_style_is($arr_value['handle'], 'registered') ){
 						
-							// TODO - register then enqueue?
-							wp_enqueue_script($arr_value['handle'], $arr_value['src'], $arr_value['deps'] ,$arr_value['ver'], $arr_value['in_footer'] );
+							wp_enqueue_style($arr_value['handle'] );
+			//echo $str_key . ' ';	
+						} elseif ( wp_script_is($arr_value['handle'], 'registered') ){
+			///echo $str_key . ' ';
+							wp_enqueue_script($arr_value['handle']);
 							
+						} else {
+						  // TODO - style must have been invalid. 
 						}
 					}
-				}			
+				  }
+	// echo $str_key . ' ';
+				}
+//die('n');				
 				return array('status' => true, 'msg' => 'success', 'source' => $str_return_source, 'arr_args' => $arr_args);
 			}
 			return array('status' => false, 'msg' => 'ERROR: arr_args !is_array()', 'source' => $str_return_source, 'arr_args' => 'error');
 		}
-
-		
-/*
- * ===============================================================================================
- * ==> wp_enqueue_web_fonts - leans on the wp_enqueue section above
- * ===============================================================================================
- */		
-
-		/**
-		 * 
-		 */
- 		public function wp_enqueue_web_fonts_validate($arr_args = NULL){
-		
-			return $this->wp_enqueue_validate($arr_args);
 	
-		}
-
-		
-		/**
-		 * 
-		 */
-		public function wp_enqueue_web_fonts_active_true($arr_args = NULL) {
-					
-			return $this->wp_enqueue_active_true($arr_args);
-
-		}
-		
-		/**
-		 *
-		 */
-		public function wp_enqueue_web_fonts_set($arr_args = NULL) {
-			$str_return_source = 'Theme \ WP Enqueue :: wp_enqueue_web_fonts_set()'; 
-
-	
-			if ( is_array($arr_args) ){
-				$this->_arr_wp_enqueue_web_fonts = $arr_args;			
-				return array('status' => true, 'msg' => 'success', 'source' => $str_return_source, 'arr_args' => $arr_args);
-			}
-			return array('status' => false, 'msg' => 'ERROR: arr_args !is_array()', 'source' => $str_return_source, 'arr_args' => 'error');
-		}
-		
-		/*
-		 *
-		 */	
-		public function add_action_wp_enqueue_web_fonts_do($str_hook = 'wp_enqueue_scripts', $int_priority = 10){
-		
-			add_action($str_hook, array( $this, 'wp_enqueue_web_fonts_do'), $int_priority = 10);
-		}
-	
-		/**
-		 *
-		 */		
-		public function wp_enqueue_web_fonts_do() {
-					
-			return $this->wp_enqueue_do($this->_arr_wp_enqueue_web_fonts);
-
-		}
-
-
-/*
- * ===============================================================================================
- * ==> wp_enqueue_scripts_and_styles - leans on the wp_enqueue section above
- * ===============================================================================================
- */	
- 
- 		/**
-		 *
-		 */	
-		public function wp_enqueue_scripts_and_styles_validate($arr_args = NULL){
-		
-			return $this->wp_enqueue_validate($arr_args);
-		} 
-		
-		/**
-		 * 
-		 */
-		public function wp_enqueue_scripts_and_styles_active_true($arr_args = NULL) {
-		
-			return $this->wp_enqueue_active_true($arr_args);
-		}
-
-		/**
-		 * 
-		 */		
-		public function wp_enqueue_scripts_and_styles_set($arr_args = NULL) {
-			$str_return_source = 'Theme \ WP Enqueue :: wp_enqueue_scripts_and_styles_set()'; 
-
-			if ( is_array($arr_args) ){
-				$this->_arr_wp_enqueue_scripts_and_styles = $arr_args;			
-				return array('status' => true, 'msg' => 'success', 'source' => $str_return_source , 'arr_args' => $arr_args);
-			}
-			return array('status' => false, 'msg' => 'ERROR: arr_args !is_array()', 'source' => $str_return_source, 'arr_args' => 'error');
-		}
-		
-		
-		/*
-		 *
-		 */	
-		public function add_action_wp_enqueue_scripts_and_styles_do($str_hook = 'wp_enqueue_scripts', $int_priority = 10){
-		
-			add_action($str_hook, array( $this, 'wp_enqueue_scripts_and_styles_do'), $int_priority = 10);
-		
-		}
-
-		/**
-		 * 
-		 */
-		public function wp_enqueue_scripts_and_styles_do() {
-							
-			return $this->wp_enqueue_do($this->_arr_wp_enqueue_scripts_and_styles);
-		}
-		
 	} // END: class
 } // END: if class exists
 ?>
